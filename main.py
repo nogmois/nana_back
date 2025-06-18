a1a406025dac807e5498a04343ba4e8961757f34
Murilo <nmurilo164@gmail.com>
2025-06-18 09:37:15 -0300

    update

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",          # React local
         "https://nanafacil.com",          # Produção futura
-        "https://www.nanafacil.com"
+        "https://www.nanafacil.com",
+        "https://nanafacil-web.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
